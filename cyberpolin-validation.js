223430b79b2272a5bceb42e25d6e2ddcf23a73e2
Carlos Gonzalez <cyberpolin@gmail.com>
2016-06-14 22:23:11 -0700

    Add basic functionallity

--- cyberpolin-validation.js
+++ cyberpolin-validation.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
   //Get the action
   $('form').on('submit', function(e){
     e.preventDefault();
-    console.log('default prevented on: ');
-    console.log($(this));
+    // Getting every item in the form
+    // TODO: catch more than one form
+    var input = ($(this).find('input[type=text]'));
+    for (var i = 0; i < input.length; i++){
+      if ($(input[i]).val().trim() == ''){
+        $(input[i]).attr('style', 'border:1px solid red');
+      } else {
+        $(input[i]).attr('style', 'border:1px solid green');
+      }
+      $(input[i]).val();
+    }
   });
 });
