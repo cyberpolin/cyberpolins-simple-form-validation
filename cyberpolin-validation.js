a0a2de0fb2aa34864fafc427f14e48814c7abcc2
Carlos Gonzalez <cyberpolin@gmail.com>
2016-06-15 17:48:05 -0700

    add errors counting and prevent default only on errors

--- cyberpolin-validation.js
+++ cyberpolin-validation.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
   //Get the action
   $('form').on('submit', function(e){
-    e.preventDefault();
     // Getting every item in the form
     // TODO: catch more than one form
+    var formErrors = 0;
     var input = ($(this).find('input[type=text]'));
     for (var i = 0; i < input.length; i++){
       if ($(input[i]).val().trim() == ''){
         $(input[i]).attr('style', 'border:1px solid red');
+        formErrors++;
+
       } else {
         $(input[i]).attr('style', 'border:1px solid green');
+
       }
       $(input[i]).val();
+    }
+    console.log(formErrors);
+    if (formErrors != 0) {
+      e.preventDefault();
     }
   });
 });
